6338a06322f544b4792206bf202cf0654fca4832
aatack <alexanderjtatack@gmail.com>
2018-11-19 15:59:34 +0000

    Add function for populating throat points

--- MoCCa/Nozzle/MinimumLength.hs
+++ MoCCa/Nozzle/MinimumLength.hs
@@ -9,11 +9,17 @@ data Point = Point { riemannInvariants :: (Double, Double)
                    , machAngle :: Double
                    , flowAngle :: Double
                    , prandtlMeyerFunction :: Double
-                   , characteristicAngles :: (Double, Double)
                    , position :: Coordinate
                    , pointType :: PointType
-                   , name :: String
                    } deriving (Show)
+
+-- | Determine the angles of the characteristic lines coming from
+-- the given point.
+characteristicAngles :: Point -> (Double, Double)
+characteristicAngles p = 
+    let theta = flowAngle p
+        mu = machAngle p
+    in (theta + mu, theta - mu)
 
 -- | Generate n equally spaced starting angles for characteristic
 -- lines between the two limits.
@@ -30,4 +36,19 @@ nozzleThroatAngle table exitMachNumber =
         Just row -> 0.5 * IFT.prandtlMeyerFunction row
         Nothing  -> error "table does not contain the requested Mach number"
 
-t = IFT.generateFlowTable 1.4 1.0 0.01
+-- | Create a point and populate its field, assuming that it is located
+-- at the top of the throat and its flow angle is given.
+createInitialPoint :: IFT.IsentropicFlowTable -> Double -> Point
+createInitialPoint table angle =
+    Point { riemannInvariants = (0, 2 * angle)
+          , machNumber = IFT.machNumber tableRow
+          , machAngle = IFT.machAngle tableRow
+          , flowAngle = angle
+          , prandtlMeyerFunction = angle
+          , position = (0, 1)
+          , pointType = Throat
+          }
+    where
+        tableRow = case IFT.lookupPrandtlMeyerFunction table angle of
+            Just r  -> r
+            Nothing -> error "table does not contain the requested P-M value"
