2af092b8e348f166eca484e1f89e5dc54f30e9f0
aatack <alexanderjtatack@gmail.com>
2018-11-20 18:37:15 +0000

    Implement function for converting CSV lists to TeX tables

--- MoCCa/Util/Lists.hs
+++ MoCCa/Util/Lists.hs
@@ -1,5 +1,8 @@
 module MoCCa.Util.Lists where
 
+import qualified Data.List as List
+import qualified Data.List.Split as Split
+
 -- | Find the values of the list either side of the point at which
 -- the given predicate first switches from True to False or vice versa,
 -- when applied element by element from the beginning of the list.
@@ -12,3 +15,9 @@ boundPredicateSwitch _ _ = Nothing
 -- | Map each value in a list to a sub-list and then join them together.
 flatMap :: (a -> [b]) -> [a] -> [b]
 flatMap f = foldl1 (++) . map f
+
+-- | Given a list of lines from a CSV file, format them as a TeX table.
+csvToTexTable :: [String] -> String
+csvToTexTable = flatMap (formatCsvLine)
+    where
+        formatCsvLine line = List.intercalate " & " (Split.splitOn "," line) ++ " \\ "
